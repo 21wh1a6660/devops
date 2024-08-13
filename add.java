f1f5812fd2a95adff73fda5927a7f57a0b370925
SHIVANI.JEEDI <96616574+21wh1a6660@users.noreply.github.com>
2024-08-13 11:49:33 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ import java.lang.*;
 class add{
 public static void main(String args[]){
 int a=10,b=20;
-System.out.print("Sum of two numbers is: " +(a+b));
+System.out.print("Sum of two numbers : " +(a+b));
 }
 }
 
